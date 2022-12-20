c2710e23c2f4031afb3b5636561e956f522c8dee
Nisal <nisalp2006@gmail.com>
2022-12-20 11:59:55 +0530

    Update loading and feedback messages

--- containers/OrganizationForms/BankDetailsForm.tsx
+++ containers/OrganizationForms/BankDetailsForm.tsx
@@ -63,8 +63,13 @@ const BankDetailsForm: FunctionComponent = () => {
                 refetch();
                 toast.success("Bank details updated successfully");
             } catch (e) {
-                let parsedErrors = (e as ApolloError).graphQLErrors;
+                let parsedError = e as ApolloError;
+                let parsedErrors = parsedError.graphQLErrors;
                 const messages = parsedErrors.map((err) => err.message);
+
+                if (parsedError && parsedError.message) {
+                    toast.error(parsedError.message);
+                }
                 
                 for (let message of messages) {
                     toast.error(message);
